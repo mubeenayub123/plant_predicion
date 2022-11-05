fdf2ba415ac28e59a6d61468786c16baf82c335b
mubeen <mubeenayub123@gmail.com>
2022-11-05 05:36:12 +0500

    flask app

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import jsonify
+import pandas as pd
+from flask import request
+from keras.models import load_model
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+     json_ = request.json
+     query_df = pd.DataFrame(json_, index=[0])
+     query = pd.get_dummies(query_df)
+     model = load_model('trained_model_finalized')
+     prediction = model.predict(query)
+     return jsonify({'prediction': prediction.tolist()})
